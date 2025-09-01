7ccb71db3de7419635c4fda90959d136cf868522
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2025-09-01 15:32:33 +0200

    feat: Add first packages & modules

--- default.nix
+++ default.nix
@@ -6,7 +6,13 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+let
+  sources = pkgs.callPackage ./_sources/generated.nix { };
+in
 
 {
   # The `lib`, `modules`, and `overlays` names are special
@@ -14,7 +20,9 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  hammerspoon = pkgs.callPackage ./pkgs/hammerspoon { source = sources.hammerspoon; };
+  paperwm-spoon = pkgs.callPackage ./pkgs/paperwm-spoon { source = sources.paperwm-spoon; };
+  powershell-beautifier = pkgs.callPackage ./pkgs/powershell-beautifier {
+    source = sources.powershell-beautifier;
+  };
 }
